b7beb7e27221618dd9ea4ec88d533860146b2dbc
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2023-11-03 18:19:18 +0300

    add new task done

--- lib/views/task/task_screen.dart
+++ lib/views/task/task_screen.dart
@@ -1,21 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+import 'package:short_point/controllers/task_controller.dart';
+import 'package:short_point/models/task_model.dart';
 import 'package:short_point/utils/constant.dart';
 
 class TaskScreen extends StatefulWidget {
-  const TaskScreen({super.key});
+  const TaskScreen({super.key, this.task, this.index});
+
+  final TaskModel? task;
+  final int? index;
 
   @override
   State<TaskScreen> createState() => _TaskScreenState();
 }
 
 class _TaskScreenState extends State<TaskScreen> {
+  final TextEditingController _taskController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _taskController.text = widget.task?.title ?? '';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: primaryColor,
-        title: const Text('Task Screen'),
-      ),
-    );
+    return Consumer<TaskController>(builder: (context, provider, _) {
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+          title: const Text(
+            'Add New Task',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SvgPicture.asset('assets/icons/arrowBack.svg'),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'Task Name',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 1.84,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _taskController,
+                decoration: InputDecoration(
+                  focusColor: primaryColor,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: primaryColor,
+                  minimumSize: Size(
+                    double.infinity,
+                    61 / screenHeight * MediaQuery.of(context).size.height,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  if (widget.task == null) {
+                    provider.addTask(_taskController.text);
+                  } else {
+                    provider.editTask(
+                      widget.index!,
+                      _taskController.text,
+                    );
+                  }
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Done',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
